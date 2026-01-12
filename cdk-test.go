080437ee0fa06b36914de4fff3d5ecf9232c3eb6
rushba <novdmitry@gmail.com>
2026-01-12 16:56:56 +0000

    Add a frotier queue and state db

--- cdk-test.go
+++ cdk-test.go
@@ -4,7 +4,9 @@ import (
 	"os"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -28,10 +30,50 @@ func NewCdkTestStack(scope constructs.Construct, id string, props *CdkTestStackP
 		AutoDeleteObjects: jsii.Bool(true),
 	})
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("CdkTestQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	// Dead-letter queue
+	dlq := awssqs.NewQueue(stack, jsii.String("UrlFrontierDLQ"), &awssqs.QueueProps{
+		RetentionPeriod: awscdk.Duration_Days(jsii.Number(14)),
+	})
+
+	// Main URL frontier queue
+	queue := awssqs.NewQueue(stack, jsii.String("UrlFrontierQueue"), &awssqs.QueueProps{
+		VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(30)),
+		DeadLetterQueue: &awssqs.DeadLetterQueue{
+			Queue:           dlq,
+			MaxReceiveCount: jsii.Number(5),
+		},
+	})
+
+	// URL state / dedup table
+	table := awsdynamodb.NewTable(stack, jsii.String("UrlStateTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("url_hash"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		BillingMode:   awsdynamodb.BillingMode_PAY_PER_REQUEST,
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
+	})
+
+	awscdk.Tags_Of(queue).Add(jsii.String("Component"), jsii.String("crawler-frontier"), nil)
+	awscdk.Tags_Of(queue).Add(jsii.String("Purpose"), jsii.String("url-ingestion"), nil)
+
+	awscdk.Tags_Of(dlq).Add(jsii.String("Component"), jsii.String("crawler-frontier"), nil)
+	awscdk.Tags_Of(dlq).Add(jsii.String("Purpose"), jsii.String("poison-messages"), nil)
+
+	awscdk.Tags_Of(table).Add(jsii.String("Component"), jsii.String("crawler-frontier"), nil)
+	awscdk.Tags_Of(table).Add(jsii.String("Purpose"), jsii.String("url-dedup-state"), nil)
+
+	awscdk.NewCfnOutput(stack, jsii.String("UrlFrontierQueueUrl"), &awscdk.CfnOutputProps{
+		Value: queue.QueueUrl(),
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("UrlFrontierDLQUrl"), &awscdk.CfnOutputProps{
+		Value: dlq.QueueUrl(),
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("UrlStateTableName"), &awscdk.CfnOutputProps{
+		Value: table.TableName(),
+	})
 
 	return stack
 }
